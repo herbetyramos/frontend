96669a3f4b0ff4eee37a734fa050cfc6f21319e5
Herbety Ramos de souza <herbetyramos@gmail.com>
2026-08-09 16:34:44 -0300

    Corrige autenticacao e configuracao da API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,35 +1,47 @@
-// src/services/api.ts
 import axios, { AxiosError } from "axios";
 import { parseCookies, destroyCookie } from "nookies";
-import Router from "next/router"; // aqui pode usar next/router porque api roda no client
-
 import { AuthTokenError } from "./errors/AuthTokenError";
 
 export function signOutGlobal() {
   destroyCookie(undefined, "@nextauth.token");
-  Router.push("/"); // força ir para login
+
+  if (typeof window !== "undefined") {
+    window.location.href = "/";
+  }
 }
 
-export function setupAPIClient(ctx = undefined) {
-  const cookies = parseCookies(ctx);
- const api = axios.create({
-  baseURL: "http://192.168.15.84:3000",
-  headers: {
-    Authorization: `Bearer ${cookies["@nextauth.token"] || ""}`,
-  },
-  withCredentials: true,
-});
+export function setupAPIClient(
+  ctx?: Parameters<typeof parseCookies>[0]
+) {
+  const api = axios.create({
+    baseURL:
+      process.env.NEXT_PUBLIC_API_URL ||
+      "http://192.168.15.84:3000",
+    withCredentials: true,
+  });
+
+  api.interceptors.request.use((config) => {
+    const cookies = parseCookies(ctx);
+    const token = cookies["@nextauth.token"];
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  });
 
   api.interceptors.response.use(
     (response) => response,
     (error: AxiosError) => {
       if (error.response?.status === 401) {
         if (typeof window !== "undefined") {
-          signOutGlobal(); // usa a função global
+          signOutGlobal();
         } else {
           return Promise.reject(new AuthTokenError());
         }
       }
+
       return Promise.reject(error);
     }
   );
--- src/services/setupAPIClient.ts
+++ src/services/setupAPIClient.ts
@@ -2,14 +2,17 @@ import axios from "axios";
 import { parseCookies } from "nookies";
 
 export function setupAPIClient() {
-  const  { "@nextauth.token": token } = parseCookies();
+  const { "@nextauth.token": token } = parseCookies();
 
   const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL:
+      process.env.NEXT_PUBLIC_API_URL ||
+      "http://192.168.15.84:3000",
+    withCredentials: true,
   });
 
   if (token) {
-    api.defaults.headers["authorization"] = `Bearer ${token}`;
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   }
 
   return api;
