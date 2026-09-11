1593a218274055011873f3f921cccda7051ba69a
Herbety Ramos de souza <herbetyramos@gmail.com>
2026-09-11 20:55:06 -0300

    ajustelistcronograma

--- src/app/listcronograma/page.tsx
+++ src/app/listcronograma/page.tsx
@@ -678,7 +678,7 @@ const visualizarSolicitacao = async (id: string) => {
           }
           className="w-full text-left px-4 py-2 hover:bg-gray-100"
         >
-          Segmento
+          Segmentos
       </button>
 
       <button
