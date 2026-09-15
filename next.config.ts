d9bd40ec5ad536e485cc147ce6c5a503d3cdf44f
Herbety Ramos de souza <herbetyramos@gmail.com>
2026-09-15 19:36:43 -0300

    onext

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    cpus: 1,
+  },
+
   images: {
     domains: ["192.168.15.84"],
   },
 };
 
 export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-
-
-
-
-
-
-
-  
-
-
