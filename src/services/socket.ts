40594650d4de0f8097a4012bd93a5c77d8bb9906
Herbety Ramos de souza <herbetyramos@gmail.com>
2026-09-15 19:20:27 -0300

    Corrige conexão Socket.IO em produção

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,5 +1,12 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
-  transports: ["websocket"],
-});
+export const socket = io(
+  typeof window !== "undefined"
+    ? window.location.origin
+    : "http://localhost:3000",
+  {
+    transports: ["websocket"],
+    autoConnect: true,
+  }
+);
+
