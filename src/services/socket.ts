f29844e59825def1211a8175d9a311e92cee2bda
Herbety Ramos de souza <herbetyramos@gmail.com>
2026-09-24 23:17:47 -0300

    fix: corrigir URL do websocket em producao

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,14 +1,12 @@
 import { io } from "socket.io-client";
 
 const socketUrl =
-  process.env.NEXT_PUBLIC_API_URL ||
-  (typeof window !== "undefined"
+  typeof window !== "undefined"
     ? window.location.origin
-    : "http://localhost:3000");
+    : "http://localhost:3000";
 
 export const socket = io(socketUrl, {
   transports: ["websocket", "polling"],
   autoConnect: true,
   withCredentials: true,
 });
-
